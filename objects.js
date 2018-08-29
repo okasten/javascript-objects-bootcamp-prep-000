0dbd395ff9e1f3c06883f0daa2b4ca11a6d9abcc
Olivia Kasten <odkasten@gmail.com>
2018-08-29 00:36:10 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {Misterwives: 'Machine'}
 function updatePlaylist(playlist, artistName, songTitle){
   playlist[artistName] = songTitle
 }
+
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName
+}
